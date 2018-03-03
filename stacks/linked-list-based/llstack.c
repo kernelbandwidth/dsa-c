75e151afa4a997973fe6b3c0863dfeea7857b130
James Crooks <crooks1379@gmail.com>
2018-03-02 18:21:29 -0800

    Implements linked-list-based stack solutions

--- stacks/linked-list-based/llstack.c
+++ stacks/linked-list-based/llstack.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 #include "llstack.h"
 
 typedef struct _StackNode {
@@ -12,36 +14,61 @@ struct _IntStackLL {
 
 IntStackLL * new_stack()
 {
-    return NULL;
+    IntStackLL * stack = malloc(sizeof *stack);
+    stack->head = NULL;
+    stack->size = 0;
+    return stack;
 }
 
 void free_stack(IntStackLL * stack)
 {
+    while (!is_empty(stack)) {
+	pop(stack);
+    }
 
+    free(stack);
 }
 
 bool is_empty(IntStackLL * stack)
 {
-    return false;
+    return 0 == stack->size;
 }
 
 size_t size(IntStackLL * stack)
 {
-    return 0;
+    size_t count = 0;
+    StackNode * current = stack->head;
+    while (NULL != current) {
+	count++;
+	current = current->next;
+    }
+
+    return count;
 }
 
 void push(IntStackLL * stack, int32_t value)
 {
-
+    StackNode * node = malloc(sizeof *node);
+    node->value = value;
+    node->next = stack->head;
+    stack->head = node;
+    stack->size++;
 }
 
 int32_t pop(IntStackLL * stack)
 {
-    return 0;
+    assert(NULL != stack->head);
+    StackNode * chead = stack->head;
+    int32_t value = chead->value;
+    stack->head = stack->head->next;
+    stack->size--;
+    free(chead);
+    return value;
 }
 
 int32_t peek(IntStackLL * stack)
 {
-    return 0;
+    assert(NULL != stack->head);
+    return stack->head->value;
 }
 
